695cd715948b9d616a6e45c8170bc7b503c086f0
Paul Adams <ghusiyy@gmail.com>
2024-03-25 20:39:29 -0700

    remove boilerplate

---
+++ src/AppDashboard.tsx
@@ -0,0 +1,19 @@
+import "./AppDashboard.css";
+
+function AppDashboard() {
+  return (
+    <div className="app-dashboard">
+      <div className="apps-grid">
+        <div>Message</div>
+        <div>Chat</div>
+        <div>Notes</div>
+        <div>Calendar</div>
+        <div>Contacts</div>
+        <div>Paint</div>
+        <div>Map</div>
+      </div>
+    </div>
+  );
+}
+
+export default AppDashboard;
