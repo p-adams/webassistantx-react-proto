a74a2265019b6f751f09fb9e4237569532bb1258
Paul Adams <ghusiyy@gmail.com>
2024-03-27 18:58:19 -0700

    add sidebar

--- src/AppDashboard/AppDashboard.tsx
+++ src/AppDashboard/AppDashboard.tsx
@@ -4,22 +4,14 @@ function AppDashboard() {
   return (
     <div className="app-dashboard">
       <div className="apps-grid">
-        {/* initial brainstorm */}
         <div>Message</div>
         <div>Personal Finance</div>
-        <div>Chat</div>
         <div>Notes</div>
         <div>News</div>
-        <div>Reminders</div>
         <div>Task Management</div>
         <div>Calendar</div>
         <div>Contacts</div>
-        <div>Travel</div>
         <div>Health and Wellness</div>
-        <div>Education</div>
-        <div>Entertainment</div>
-        <div>Paint</div>
-        <div>Map</div>
       </div>
     </div>
   );
---
+++ src/AppSidebar/index.tsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+
+const Sidebar = () => {
+  return (
+    <nav className="sidebar">
+      <div className="logo">
+        <img src="logo.png" alt="WebAssistantX Logo" />
+      </div>
+      <ul className="nav-links">
+        <li>
+          <Link to="/message">Message</Link>
+        </li>
+        <li>
+          <Link to="/calendar">Calendar</Link>
+        </li>
+        <li>
+          <Link to="/tasks">Task Management</Link>
+        </li>
+        <li>
+          <Link to="/notes">Notes</Link>
+        </li>
+        <li>
+          <Link to="/contacts">Contacts</Link>
+        </li>
+        <li>
+          <Link to="/finance">Personal Finance</Link>
+        </li>
+        <li>
+          <Link to="/health">Health and Wellness</Link>
+        </li>
+        <li>
+          <Link to="/news">News</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Sidebar;
