65c83557219b0191662552ed7a3f0b6a2be00e3c
Paul Adams <ghusiyy@gmail.com>
2024-04-03 22:59:38 -0700

    add links to dashboard

--- src/routes/AppDashboard/AppDashboard.tsx
+++ src/routes/AppDashboard/AppDashboard.tsx
@@ -1,16 +1,33 @@
+import { Link } from "react-router-dom";
 import "./AppDashboard.css";
 function AppDashboard() {
   return (
     <section className="app-dashboard">
       <div className="app-grid">
-        <div className="app-item">Message</div>
-        <div className="app-item">Calendar</div>
-        <div className="app-item">Task Management </div>
-        <div className="app-item">Notes</div>
-        <div className="app-item">Contacts</div>
-        <div className="app-item">Personal Finance</div>
-        <div className="app-item">Health and Wellness</div>
-        <div className="app-item">News</div>
+        <div className="app-item">
+          <Link to="/message">Message</Link>{" "}
+        </div>
+        <div className="app-item">
+          <Link to="/calendar"></Link> Calendar
+        </div>
+        <div className="app-item">
+          <Link to="/tasks">Task Management </Link>
+        </div>
+        <div className="app-item">
+          <Link to="/notes">Notes</Link>
+        </div>
+        <div className="app-item">
+          <Link to="/contacts">Contacts</Link>
+        </div>
+        <div className="app-item">
+          <Link to="/finance">Personal Finance</Link>
+        </div>
+        <div className="app-item">
+          <Link to="/health">Health and Wellness</Link>
+        </div>
+        <div className="app-item">
+          <Link to="/news">News</Link>
+        </div>
       </div>
     </section>
   );
