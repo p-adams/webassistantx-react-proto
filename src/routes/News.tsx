3e1df6a94308306ecc1b0814941b73a1108395c2
Paul Adams <ghusiyy@gmail.com>
2024-04-16 00:38:41 -0700

    display news headlines

--- src/routes/News.tsx
+++ src/routes/News.tsx
@@ -1,5 +1,44 @@
+import { useEffect, useState } from "react";
+
+interface NewsData {
+  author: string;
+  content: string;
+  description: string;
+  publishedAt: string | Date;
+  source: {
+    id: string;
+    name: string;
+  };
+  title: string;
+  url: string;
+  urlToImage: string;
+}
+
+const key = import.meta.env.VITE_NEWS_API_KEY;
+
+const fetchNewsData = async (key: string) =>
+  await fetch(
+    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${key}`
+  ).then((res) => res.json());
+
 function News() {
-  return <div>news</div>;
+  const [data, setData] = useState<NewsData[]>([]);
+  useEffect(() => {
+    fetchNewsData(key)
+      .then((res) => setData(res.articles))
+      .catch((err) => console.log(err));
+  }, [setData]);
+
+  return (
+    <div>
+      news
+      <ul>
+        {data.map((obj) => (
+          <li key={obj.title}>{obj.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default News;
